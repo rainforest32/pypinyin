1dd4a526199364299d1c6f4e95fe65d57c4037fe
smallqiao <dlutwy@qq.com>
2014-05-17 23:54:25 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup 
+setup(name='pypinyin',
+      version='0.1',
+      description='a python project for getting pinyin for Chinese words or sentence',
+      author='Small Qiao',
+      author_email='dlutwy@qq.com',
+      url='http://www.smallqiao.com',
+      package_dir={'pypinyin':'pypinyin'},
+      py_modules=['jieba']
+) 
