646b6fa403c55b822ba7720fff39bd00fe4396f8
smallqiao <dlutwy@qq.com>
2014-05-17 23:28:30 +0800

    import source

---
+++ test.py
@@ -0,0 +1,20 @@
+#coding:gbk
+
+import sys, pypinyin
+
+def main():
+    getPy = pypinyin.GetPinyin()
+    getPy.load()
+    print 'text getPy: '
+    results = getPy.getPy(u"wo今天要抢红米，重庆是个好地方")
+    for result in results:
+        print result.py
+    
+    print 'test getMaxPy: '
+    print getPy.getMaxPy(u'wo今天要抢红米，重庆是个好地方，我热爱江西')
+    print getPy.getMaxPy(u'我是中国人，我热爱我的祖国，遇到重大事情，请报告组织')
+    return 0
+    
+
+if __name__ == '__main__':
+    sys.exit(main())
